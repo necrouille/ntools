b3e0d422999c2a9a28fb0f81312bc1d31212339f
necro <necro@THANATOS>
2025-06-15 21:28:23 +0200

    Rework script for series filtering

--- bash/jellyfin_file_organizer.sh
+++ bash/jellyfin_file_organizer.sh
@@ -12,49 +12,16 @@ SERIE_DIR="Series"
 
 # Extensions to consider
 MEDIA_EXT="mp4|mkv|avi|mov"
-IGNORE_EXT="meta|nfo"
-SRT_EXT="srt"
-
-# Directories to ignore
-IGNORE_DIR="Apps|rclone-mnt|watch|OpenVPN|Series|Movies"
-
-# Function to normalize series name
-normalize_series_name() {
-    local name="$1"
-    # Convert to lowercase
-    name=$(echo "$name" | tr '[:upper:]' '[:lower:]')
-    # Replace dots, underscores, and multiple spaces with single space
-    name=$(echo "$name" | sed -E 's/[._]+/ /g' | sed -E 's/[[:space:]]+/ /g')
-    # Trim leading and trailing spaces
-    name=$(echo "$name" | sed -E 's/^[[:space:]]+|[[:space:]]+$//g')
-    echo "$name"
-}
-
-# Function to extract season and episode numbers
-extract_season_episode() {
-    local filename="$1"
-    local season=""
-    local episode=""
-    
-    # Try S01E02 format
-    if [[ "$filename" =~ S([0-9]{1,2})E([0-9]{1,3}) ]]; then
-        season="${BASH_REMATCH[1]}"
-        episode="${BASH_REMATCH[2]}"
-    # Try 1x02 format
-    elif [[ "$filename" =~ ([0-9]{1,2})x([0-9]{1,3}) ]]; then
-        season="${BASH_REMATCH[1]}"
-        episode="${BASH_REMATCH[2]}"
-    fi
-    
-    # Pad season and episode numbers with zeros
-    season=$(printf "%02d" "$season")
-    episode=$(printf "%02d" "$episode")
-    
-    echo "$season:$episode"
-}
 
 # List all media files and ignore other files/folders
-find "$SOURCE_DIR" -type f -regextype posix-extended -regex ".*\.($MEDIA_EXT)$" ! -regex ".*\.($IGNORE_EXT)$" -not -path "*/\($IGNORE_DIR\)/*" | while read -r file; do
+find "$SOURCE_DIR" -type f -regextype posix-extended -regex ".*\.($MEDIA_EXT)$" \
+        -not -path "*/Apps/*" \
+        -not -path "*/watch/*" \
+        -not -path "*/rclone-mnt/*" \
+        -not -path "*/OpenVPN/*" \
+        -not -path "*/Series/*" \
+        -not -path "*/Movies/*" \
+        | while read -r file; do
   # For each file, check of it is a serie, if not it is a movie
   # Detection process :
   # - Check if the file is a serie : if it contains S[0-9]{1,2}E[0-9]{1,2} or [0-9]{1,2}x[0-9]{1,2}
@@ -68,39 +35,92 @@ find "$SOURCE_DIR" -type f -regextype posix-extended -regex ".*\.($MEDIA_EXT)$" 
   # - 1x12 : 1-2 digits (season) + x + 1-3 digits (episode)
   # - Season 1 : "Season" + optional space + 1-2 digits (season)
   # - Saison 1 : "Saison" + optional space + 1-2 digits (season)
-  if echo "$filename" | grep -Eiq 'S[0-9]{1,2}E[0-9]{1,3}|[0-9]{1,2}x[0-9]{1,3}|Season[ ]*[0-9]{1,2}|Saison[ ]*[0-9]{1,2}|VOL[ ]*[0-9]{1,2}|Vol[ ]*[0-9]{1,2}|vol[ ]*[0-9]{1,2}|Volume[ ]*[0-9]{1,2}|volume[ ]*[0-9]{1,2}|- [0-9]{1,2} '; then
+  if echo "$filename" | grep -Eiq 'S[0-9]{1,2}E[0-9]{1,3}|[0-9]{1,2}x[0-9]{1,3}|Season[._ ][0-9]{1,2}|Saison[._ ][0-9]{1,2}|VOL[0-9]{1,2}|Vol[._ ][0-9]{1,2}|vol[._ ][0-9]{1,2}|Volume[._ ][0-9]{1,2}|volume[._ ][0-9]{1,2}|- [0-9]{1,2} '; then
     # Serie section
     
-    # Extract series name from path or filename
-    series_name=""
-    if [[ "$dirname" != "$SOURCE_DIR" ]]; then
-        # Try to get series name from directory
-        series_name=$(basename "$dirname")
+    if echo "$filename" | grep -Eiq '[sS][0-9]{1,2}'; then
+      # Reset dirname
+      series_name=$(echo "$filename" | sed -E 's/[._ ][sS][0-9]{1,2}.*$//i' | sed -e 's/ /\./g')
+      [ ! -d "$SOURCE_DIR/$SERIE_DIR/$series_name" ] && mkdir "$SOURCE_DIR/$SERIE_DIR/$series_name"
+      dirname=$(dirname "$file" | sed -e "s|^$SOURCE_DIR|../..|g")
+      # Set src and target for symlink
+      src_file="$dirname/$filename"
+      target="$SERIE_DIR/$series_name/$filename"
+      # Check if the symlink file already exists in the target directory
+      if [ ! -e "$SOURCE_DIR/$SERIE_DIR/$series_name/$filename" ]; then
+        # If not, create a symlink to the source file
+        ln -s "$src_file" "$target"
+      fi
+
+    elif echo "$filename" | grep -Eiq '[0-9]{1,2}x[0-9]{1,3}'; then
+      # Reset dirname
+      if echo "$dirname" | grep -Eiq 'titans'; then
+        series_name="L'attaque.des.titans"
+      elif echo "$dirname" | grep -Eiq '[Ss][._ ]*[0-9]{1,2}'; then
+        series_name=$(echo "$dirname" | sed -E 's/[._ ][sS][0-9]{1,2}.*$//i' | sed -e 's/^..\///g' | sed -e 's/ /\./g')
+      fi
+      [ ! -d "$SOURCE_DIR/$SERIE_DIR/$series_name" ] && mkdir "$SOURCE_DIR/$SERIE_DIR/$series_name"
+      dirname=$(dirname "$file" | sed -e "s|^$SOURCE_DIR|../..|g")
+      # Set src and target for symlink
+      src_file="$dirname/$filename"
+      target="$SERIE_DIR/$series_name/$filename"
+      # Check if the symlink file already exists in the target directory
+      if [ ! -e "$SOURCE_DIR/$SERIE_DIR/$series_name/$filename" ]; then
+        # If not, create a symlink to the source file
+        ln -s "$src_file" "$target"
+      fi
+
+    elif echo "$filename" | grep -Eiq 'VOL[0-9]{1,2}'; then
+      # Reset dirname
+      if echo "$dirname" | grep -Eiq 'titans'; then
+        series_name=$(echo "$dirname" | sed -E 's/[._ ][Ss]aison[._ ][0-9]{1,2}.*$//i' | sed -e 's|../||g' | sed -e 's/ /\./g')
+      elif echo "$dirname" | grep -Eiq 'VOL[._ ]*[0-9]{1,2}'; then
+        series_name=$(echo "$dirname" | sed -E 's/[._ ]VOL[0-9]{1,2}.*$//i' | sed -e 's|../||g' | sed -e 's/ /\./g')
+      elif echo "$dirname" | grep -Eiq 'Saison[ ]*[0-9]{1,2}'; then
+        series_name=$(echo "$dirname" | sed -E 's/[._ ][Ss]aison[._ ][0-9]{1,2}.*$//i' | sed -e 's|../||g' | sed -e 's/ /\./g')
+      fi
+      dirname=$(dirname "$file" | sed -e "s|^$SOURCE_DIR|../..|g")
+      [ ! -d "$SOURCE_DIR/$SERIE_DIR/$series_name" ] && mkdir "$SOURCE_DIR/$SERIE_DIR/$series_name"
+      # Set src and target for symlink
+      src_file="$dirname/$filename"
+      target="$SERIE_DIR/$series_name/$filename"
+      # Check if the symlink file already exists in the target directory
+      if [ ! -e "$SOURCE_DIR/$SERIE_DIR/$series_name/$filename" ]; then
+        # If not, create a symlink to the source file
+        ln -s "$src_file" "$target"
+      fi
+
+    elif echo "$filename" | grep -Eiq '[0-9]{1,2} '; then
+      # Specific use case : Check if folder exist and if there is more than 2 media files into the folder to confirm the serie
+      if [ $(find $(dirname "$file") -maxdepth 1 -type f -regextype posix-extended -regex ".*\.($MEDIA_EXT)$" ! -regex ".*\.($IGNORE_EXT)$" -not -path "*/\($IGNORE_DIR\)/*" | wc -l) -gt 2 ]; then
+        # At least to media files so, it's a serie
+        series_name=$(dirname "$file" | sed -e "s|^$SOURCE_DIR/||g" | sed -E 's/[._][ ]-[ ][0-9]{1,2} .*$//i' | sed -e 's/ /\./g')
+        [ ! -d "$SOURCE_DIR/$SERIE_DIR/$series_name" ] && mkdir "$SOURCE_DIR/$SERIE_DIR/$series_name"
+
+        # Set src and target for symlink
+        dirname=$(dirname "$file" | sed -e "s|^$SOURCE_DIR|../..|g")
+        src_file="$dirname/$filename"
+        target="$SERIE_DIR/$series_name/$filename"
+
+        # Check if the symlink file already exists in the target directory
+        if [ ! -e "$SOURCE_DIR/$SERIE_DIR/$series_name/$filename" ]; then
+          # If not, create a symlink to the source file
+          ln -s "$src_file" "$target"
+        fi
+      else
+        # not a serie but movie
+        # Set src and target for symlink    
+        src_file="$dirname/$filename"
+        target="$FILM_DIR/$filename"
+
+        # Check if the symlink file already exists in the target directory
+        if [ ! -e "$target" ]; then
+          # If not, create a symlink to the source file
+          ln -s "$src_file" "$target"
+        fi
+      fi
     else
-        # Extract from filename (remove season/episode info)
-        series_name=$(echo "$filename" | sed -E 's/[._]S[0-9]{1,2}E[0-9]{1,3}.*$//i' | sed -E 's/[._][0-9]{1,2}x[0-9]{1,3}.*$//i')
-    fi
-    
-    # Normalize series name
-    normalized_name=$(normalize_series_name "$series_name")
-    
-    # Extract season and episode
-    season_episode=$(extract_season_episode "$filename")
-    season=$(echo "$season_episode" | cut -d':' -f1)
-    episode=$(echo "$season_episode" | cut -d':' -f2)
-    
-    # Create series directory if it doesn't exist
-    series_dir="${SERIE_DIR}/${normalized_name}"
-    [ ! -d "$series_dir" ] && mkdir -p "$series_dir"
-    
-    # Create season directory if it doesn't exist
-    season_dir="${series_dir}/Season_${season}"
-    [ ! -d "$season_dir" ] && mkdir -p "$season_dir"
-    
-    # Create symlink for the episode
-    target="${season_dir}/${normalized_name}_S${season}E${episode}.${filename##*.}"
-    if [ ! -e "$target" ]; then
-        ln -s "$file" "$target"
+      echo "unknown filter"
     fi
     
   else
